85a4e86faf6f36a6037dbb00c888d4820eaaf0be
sanjay-dev-patidar <142602180+sanjay-dev-patidar@users.noreply.github.com>
2024-06-29 20:34:07 +0530

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ const { sendWelcomeEmail } = require('./mailer');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:5000/api/auth/google/callback",
+  callbackURL: "https://eventease-api.onrender.com/api/auth/google/callback",
   passReqToCallback: true
 },
 async (req, accessToken, refreshToken, profile, done) => {
